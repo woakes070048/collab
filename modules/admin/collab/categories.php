dd8f49fcda8ef7d922fd94d1a3b89db8c50b247e
kcarwile <kcarwile@gmail.com>
2015-11-14 14:01:34 -0800

    collab content wizard configuration

--- modules/admin/collab/categories.php
+++ modules/admin/collab/categories.php
@@ -48,30 +48,75 @@ class _categories extends \IPS\Node\Controller
 	protected function manageApp()
 	{
 		$category = \IPS\collab\Category::load( \IPS\Request::i()->cat );
+		$lang = \IPS\Member::loggedIn()->language();
 	
 		foreach ( \IPS\collab\Application::collabOptions() as $app => $nodes )
 		{
 			if ( $app == \IPS\Request::i()->mapp )
-			{
-				$form = new \IPS\Helpers\Form( 'collab_category_app_config' );
-				$form->addHtml( "<h1>" . $category->_title . "</h1><hr class='ipsHr' />" );
-				
-				/* Node Options */
-				foreach ( $nodes as $option )
-				{
-					$category->addNodeOption( $form, $option );
+			{				
+				if ( \IPS\Request::i()->nid )
+				{					
+					/* Configure a specific content type */
+					foreach( $nodes as $option )
+					{
+						if ( \IPS\Request::i()->nid == md5( $option[ 'node' ] ) )
+						{
+							$steps = $category->contentConfigSteps( NULL, $option );
+							\IPS\Output::i()->output = new \IPS\Helpers\Wizard( $steps, \IPS\Http\Url::internal( "app=collab&module=collab&controller=categories&do=manageApp" )->setQueryString( array( 'cat' => $category->_id, 'mapp' => $app, 'nid' => \IPS\Request::i()->nid ) ), TRUE, array() );
+							return;
+						}
+					}
 				}
-				
-				if ( $values = $form->values() )
-				{
-					$category->updateNodeSettings( $values );
-					$category->save();
-					\IPS\Output::i()->redirect( \IPS\Http\Url::internal( "app=collab&module=collab&controller=categories" ), 'collab_category_updated' );
+				else
+				{					
+					if ( \IPS\Settings::i()->collab_category_longconfig )
+					{
+						$form = new \IPS\Helpers\Form( 'collab_category_app_config' );					
+						$form->addHtml( "<h1>" . $category->_title . "</h1><hr class='ipsHr' />" );					
+					}
+					else
+					{
+						$form = new \IPS\Helpers\Form( 'collab_category_app_config', 'done' );					
+						$form->addHtml( "<h1>" . $category->_title . "</h1><hr class='ipsHr' />" );
+						$form->hiddenValues[ 'complete' ] = 1;					
+					}
+					
+					foreach ( $nodes as $option )
+					{
+						if ( \IPS\Settings::i()->collab_category_longconfig )
+						{
+							/* Show all-in-one config */
+							$steps = $category->contentConfigSteps( $form, $option );
+							
+							call_user_func( $steps[ 'limits' ] );
+							call_user_func( $steps[ 'permissions' ] );
+							call_user_func( $steps[ 'moderation' ] );
+						}
+						else
+						{
+							/* Link to wizard config */
+							$nid			= md5( $option[ 'node' ] );
+							$nodeClass 		= $option[ 'node' ];
+							$nodeTitle 		= ucwords( $lang->get( $nodeClass::$nodeTitle ) );
+							$contentClass		= $option[ 'content' ];
+							$contentTitle 		= $contentClass ? ucwords( $lang->get( $contentClass::$title ) ) : NULL;
+
+							$wizardurl = \IPS\Http\Url::internal( "app=collab&module=collab&controller=categories&do=manageApp" )->setQueryString( array( 'cat' => $category->_id, 'mapp' => $app, 'nid' => $nid, '_new' => 1 ) );
+							$form->addHtml( "<h2 class='ipsFieldRow_section'>{$contentTitle} {$nodeTitle} &nbsp;&nbsp;<i class='fa fa-caret-right'></i> <a href='{$wizardurl}' class='ipsButton_verySmall'><i class='fa fa-magic'></i> " . $lang->addToStack( 'collab_configureWizard' ) . "</a></h2>" );
+						}
+					}
+					
+					if ( $values = $form->values() )
+					{
+						$category->updateNodeSettings( $values );
+						$category->save();
+						\IPS\Output::i()->redirect( \IPS\Http\Url::internal( "app=collab&module=collab&controller=categories" ), 'collab_category_updated' );
+					}
+					
+					\IPS\Output::i()->title = \IPS\Member::loggedIn()->language()->addToStack( 'collab_category_app_config' );
+					\IPS\Output::i()->output .= $form;
+					return;
 				}
-				
-				\IPS\Output::i()->title = \IPS\Member::loggedIn()->language()->addToStack( 'collab_category_app_config' );
-				\IPS\Output::i()->output .= $form;
-				return;
 			}
 		}		
 		
