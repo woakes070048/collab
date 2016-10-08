241157e207bcb668d3ef49ed5108577352ab86e6
kcarwile <kcarwile@gmail.com>
2016-10-08 09:57:38 -0700

    refactored code to allow collab forums to be created at the root level

--- hooks/modForumsForum.php
+++ hooks/modForumsForum.php
@@ -42,10 +42,11 @@ class collab_hook_modForumsForum extends _HOOK_CLASS_
 		parent::form( $form );
 		
 		/**
-		 * Limit theme selection in collabs to only those which the user can actually use
+		 * Special Cases
 		 */
 		if ( \IPS\collab\Application::affectiveCollab() )
 		{
+			/* Look for theme select element and modify it to use only themes that the user has access to */
 			foreach( $form->elements[ 'forum_settings' ] as &$formElement )
 			{
 				if ( $formElement instanceof \IPS\Helpers\Form\FormAbstract and $formElement->name == 'forum_skin_id' )
@@ -72,7 +73,40 @@ class collab_hook_modForumsForum extends _HOOK_CLASS_
 					$formElement = new \IPS\Helpers\Form\Select( 'forum_skin_id', $this->id ? $this->skin_id : 0, FALSE, array( 'options' => $themes ), NULL, NULL, NULL, 'forum_skin_id' );
 				}
 			}
+			
+			/* Allow forums to be saved without a category as a parent */
+			if ( isset( $form->elements[ 'forum_settings' ][ 'forum_type' ] ) and isset( $form->elements[ 'forum_settings' ][ 'forum_parent_id' ] ) )
+			{
+				$forum_type = $form->elements[ 'forum_settings' ][ 'forum_type' ];
+				$forum_parent = $form->elements[ 'forum_settings' ][ 'forum_parent_id' ];
+				
+				/* Clear any potential "category required" error */
+				if ( $forum_type->value == 'normal' and $forum_parent->value == 0 )
+				{
+					$forum_parent->error = NULL;
+				}
+			}
 		}
+	}
+	
+	/**
+	 * [Node] Format form values from add/edit form for save
+	 *
+	 * @param	array	$values	Values from the form
+	 * @return	array
+	 */
+	public function formatFormValues( $values )
+	{
+		if ( \IPS\collab\Application::affectiveCollab() )
+		{
+			/* Core code in IPS 4.1.15+ will change forums without a parent to a category if we don't do this */
+			if ( isset( $values[ 'forum_parent_id' ] ) and $values[ 'forum_parent_id' ] === 0 and $values[ 'forum_type' ] == 'normal' )
+			{
+				$values[ 'forum_parent_id' ] = -1;
+			}
+		}
+		
+		return parent::formatFormValues( $values );
 	}
 
 }
